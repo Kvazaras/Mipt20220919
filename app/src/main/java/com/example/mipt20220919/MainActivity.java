063d77445efa3785dc841512f4da31f4d816fb58
kvazaras <martas.ambraziunas@gmail.com>
2022-09-19 08:44:06 +0100

    Add change text button

--- app/src/main/java/com/example/mipt20220919/MainActivity.java
+++ app/src/main/java/com/example/mipt20220919/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mipt20220919;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = (TextView)findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        tvMain.setText("Hello");
     }
 }
